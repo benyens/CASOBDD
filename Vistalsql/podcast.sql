7d77c76df09ed7157fa07a54ccad5dcc97ff7ec6
bferrada <bferrada@alumnos.inf.utfsm.cl>
2024-07-06 19:42:06 -0400

    Último aviso Gaete se le viene la Sate

--- Vistalsql/podcast.sql
+++ Vistalsql/podcast.sql
@@ -1,2 +1,23 @@
 CREATE VIEW PODCAST_VIEW AS
-SELECT PODCAST.id, PODCAST.nombre, PODCAST.descripcion, PODCAST.fecha_creacion, USUARIO.nombre AS usuario
+SELECT
+    PODCAST.id,
+    PODCAST.nombre,
+    PODCAST.seguido ,
+    PODCAST.calificacion ,
+    PODCAST.descripcion ,
+    CANAL.id,
+    CANAL.nombre,
+    EPISODIO.id,
+    EPISODIO.nombre,
+    EPISODIO.descripcion,
+    EPISODIO.descargado,
+    EPISODIO.agregado,
+    EPISODIO.fecha_publicacion,
+    EPISODIO.duracion,
+    GENERO_PODCAST.id,
+    GENERO_PODCAST.nombre
+FROM PODCAST
+LEFT JOIN CANAL ON PODCAST.id = CANAL.PODCAST_id
+LEFT JOIN EPISODIO ON PODCAST.id = EPISODIO.PODCAST_id
+LEFT JOIN GENERO_PODCAST ON PODCAST.id = GENERO_PODCAST.PODCAST_id
+LEFT JOIN GENERO ON GENERO_PODCAST.nombre_genero = GENERO.id;
